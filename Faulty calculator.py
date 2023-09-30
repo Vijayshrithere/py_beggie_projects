9befb1893d28163689e0192c1f88c1a9fe15017a
Vijayshri There <146453274+Vijayshrithere@users.noreply.github.com>
2023-09-30 16:26:43 +0530

    Update and rename E3.py to Faulty calculator.py

---
+++ Faulty calculator.py
@@ -0,0 +1,23 @@
+## This is a Faulty Calculator which works properly for all the other numbers except for 45*3 , 56+9 , 56/6
+
+print("enter no 1: ")
+inp1=int(input())
+print("enter no 2: ")
+inp2=int(input())
+print("choose operation,*,+,/ : ")
+operation=input()
+
+if inp1 ==45 and inp2 ==3 and operation =='*':
+    print(555)
+elif operation =='*' and inp1!=45 and inp2!=3:
+    print(inp1*inp2)
+
+if inp1 ==56 and inp2 ==9 and operation =='+':
+    print(77)
+elif operation =='+' and inp1!=56 and inp2!=9:
+    print(inp1+inp2)
+
+if inp1 ==56 and inp2 ==6 and operation =='/':
+    print(4)
+elif operation == '/' and inp1!=56 and inp2!=6:
+    print(inp1 / inp2)
